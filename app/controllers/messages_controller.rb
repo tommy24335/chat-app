347c8b2817bd4fd599858be92416a60512ab8940
tommy24335 <a12799866@gmail.com>
2021-04-26 20:34:34 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,25 @@
 class MessagesController < ApplicationController
+  # form_withに必要な情報を取得
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  # messagesコントローラーにcreateアクションを定義します。
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    # メッセージを保存できた場合とできなかった場合で条件分岐の処理
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+  # privateメソッドとしてmessage_paramsを定義し、メッセージの内容contentをmessagesテーブルへ保存できるようにします。
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"rooms#index"
   resources :users, only: [:edit, :update]
+  
   # 新規チャットルームの作成で動くアクションは「new」と「create」
   resources :rooms, only: [:new, :create]
+  
+  # メッセージ送信機能に必要なindexとcreateのルーティング
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
